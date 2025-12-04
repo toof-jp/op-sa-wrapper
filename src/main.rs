ec674747e58f48419e3e1f9505ef394a3b3d4821
Fumiya Tokumasu <toof@toof.jp>
2025-12-04 20:16:14 +0900

    feat: cargo init

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::process::Command;
+
+fn load_op_service_account_token() -> String {
+    let gpg_tty = std::process::Command::new("tty")
+        .output()
+        .expect("tty failed");
+
+    let gpg_tty = String::from_utf8_lossy(&gpg_tty.stdout).trim().to_string();
+
+    let home = std::env::var("HOME").expect("HOME no set");
+    let token_path = format!("{}/.secret/op_service_account_token.gpg", home);
+    let output = Command::new("gpg")
+        .args(["--decrypt", &token_path])
+        .env("GPG_TTY", &gpg_tty)
+        .output()
+        .expect("failed to decrypt token");
+
+    assert!(
+        output.status.success(),
+        "gpg failed to decrypt op service account token",
+    );
+
+    String::from_utf8_lossy(&output.stdout).trim().to_string()
+}
+
+fn main() {
+    let op_service_account_token = load_op_service_account_token();
+    dbg!("start");
+
+    let mut args = std::env::args().skip(1);
+
+    let op_path = "/usr/bin/op";
+    let mut cmd = Command::new(op_path);
+
+    if let Some(command) = args.next() {
+        let args: Vec<String> = args.collect();
+
+        if command == "run" {
+            if let Some(pos) = args.iter().position(|s| s == "--") {
+                let flags = args[..pos].to_vec();
+                let commands = args[pos + 1..].to_vec();
+
+                dbg!(&flags, &commands);
+
+                cmd.arg("run");
+                cmd.args(flags);
+                cmd.arg("--");
+                cmd.arg("sh");
+                cmd.arg("-c");
+                cmd.arg(format!(
+                    "unset OP_SERVICE_ACCOUNT_TOKEN && {}",
+                    commands.join(" ")
+                ));
+            }
+        } else {
+            cmd.args(args);
+        }
+    }
+
+    dbg!(&cmd);
+    let status = cmd
+        .env("OP_SERVICE_ACCOUNT_TOKEN", op_service_account_token)
+        .status()
+        .expect("failed to execute op");
+
+    if let Some(code) = status.code() {
+        std::process::exit(code);
+    }
+}
